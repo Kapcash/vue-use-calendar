c9be9c0340c8d2e10bb173b3c8435f0895062644
Florent Catiau-Tristant <florent.ct@orange.fr>
2022-01-30 17:16:17 +0100

    fix: infinite month generation

--- lib/use-monthly-calendar.ts
+++ lib/use-monthly-calendar.ts
@@ -40,28 +40,35 @@ export function monthlyCalendar<C extends ICalendarDate>(globalOptions: Normaliz
       if (!newMonth) {
         const newMonthYear = currentMonth.value.days[10].monthYearIndex + (isNext ? 1 : -1);
         const newMonth = generateMonth(newMonthYear, !!otherMonthDays, globalOptions.firstDayOfWeek) as Month<C>;
-        daysByMonths.push(newMonth);
+        isNext ? daysByMonths.push(newMonth) : daysByMonths.unshift(newMonth);
       }
     }
 
-    function moveMonth (offset: number) {
-      return function nextMonth () {
-        if (infinite) {
-          genMonth(currentMonthIndex.value + offset);
-        }
-        if (nextMonthEnabled.value) {
-          currentMonthIndex.value += offset;
-        }
-      };
+    function nextMonth () {
+      const newMontIndex = currentMonthIndex.value + 1;
+      if (infinite) {
+        genMonth(newMontIndex);
+      }
+      if (nextMonthEnabled.value) {
+        currentMonthIndex.value = newMontIndex;
+      }
     }
 
-    const nextMonth = moveMonth(1);
-    const prevMonth = moveMonth(-1);
+    function prevMonth () {
+      const newMontIndex = currentMonthIndex.value - 1;
+      if (infinite) {
+        genMonth(newMontIndex);
+      }
+      if (prevMonthEnabled.value) {
+        currentMonthIndex.value = Math.max(0, newMontIndex);
+      }
+    }
 
     const computeds = useComputeds(days);
 
     return {
       currentMonth,
+      currentMonthIndex,
       months: daysByMonths,
       days,
       nextMonth,
